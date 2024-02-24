0afb222c62759557d67247544ea104fa87f7cf20
Nilushiya <nilushiyak@gmail.com>
2024-02-24 21:38:38 +0530

    BestSale component finised

--- frontend/src/Components/BestSales/BestSales.jsx
+++ frontend/src/Components/BestSales/BestSales.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import { Link } from 'react-router-dom';
 import './BestSales.css'
 // import CartdIcon from '../Assets/card.png'
 
@@ -27,9 +28,9 @@ const BestSales = () => {
       
 
         const simulatedBestSalesHim = [
-            { image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'A' , count : '65' },
-            { image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'A' , count : '95'},
-            { image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A', count : '25' }
+            {id : '21' , image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'A' , count : '65' },
+            {id : '10' , image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'A' , count : '95'},
+            {id : '21' , image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A', count : '25' }
           ];
       
           
@@ -37,9 +38,9 @@ const BestSales = () => {
 
 
           const simulatedBestSalesHer = [
-            { image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'B' , count : '365' },
-            { image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'B' , count : '95'},
-            { image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'B', count : '125' }
+            {id : '12' , image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'B' , count : '365' },
+            {id : '14' , image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'B' , count : '95'},
+            {id : '1' , image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'B', count : '125' }
           ];
       
           
@@ -53,12 +54,20 @@ const BestSales = () => {
                     <div className='BestSale'>
                         <h4>BEST SALES</h4>
                         <h2>FOR HIM</h2>
-                        <div className="row d-flex justify-content-center align-items-center m-0">
+                        <div className="row best d-flex justify-content-center align-items-center m-0">
                         {HimbestSales
                             .map((HimbestSale, index) => (
-                            <div key={index} className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center BestSaleImg">
-                                <img src={HimbestSale.image} className="img-fluid m-2" alt="" />
-                            </div>
+                                <div key={index} className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center BestSaleImg">
+                                <Card  >
+                                <Link to={`/product/${HimbestSale.id}`}><Card.Img className='cardImg img-fluid ' variant="top" src={HimbestSale.image} /></Link>
+                                  {/* <Card.Link href="#"> <img src={CartdIcon} alt=""  style={{ width: '50px', height: '50px' }}/></Card.Link> */}
+                                  <Card.Body className='cardBody'>
+                                      <Card.Title className='cardTitle'>{HimbestSale.count}</Card.Title>
+                                      <Card.Title className='cardTitle'>{HimbestSale.gender}</Card.Title>
+                                      {/* <Card.Link href="#">Card Link</Card.Link> */}
+                                  </Card.Body>
+                                  </Card>
+                          </div>
                             ))}
                         </div>
                     </div>
@@ -67,16 +76,16 @@ const BestSales = () => {
                     <div  className='BestSale'>
                         <h4>BEST SALES</h4>
                         <h2>FOR HER</h2>
-                        <div className="row d-flex justify-content-center align-items-center m-0">
+                        <div className="row best d-flex justify-content-center align-items-center m-0">
                         {HerbestSales
                             .map((HerbestSale, index) => (
                             <div key={index} className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center BestSaleImg">
-                                  <Card  style={{ width: '19rem' }}>
-                                    <Card.Img className='' variant="top" src={HerbestSale.image} style={{ width: '304px', height: '300px' }}/>
+                                  <Card className='card' >
+                                  <Link to={`/product/${HerbestSale.id}`}><Card.Img className='cardImg img-fluid ' variant="top" src={HerbestSale.image} /></Link>
                                     {/* <Card.Link href="#"> <img src={CartdIcon} alt=""  style={{ width: '50px', height: '50px' }}/></Card.Link> */}
-                                    <Card.Body>
-                                        <Card.Title>{HerbestSale.count}</Card.Title>
-                                        <Card.Title>{HerbestSale.gender}</Card.Title>
+                                    <Card.Body className='cardBody'>
+                                        <Card.Title className='cardTitle'>{HerbestSale.count}</Card.Title>
+                                        <Card.Title className='cardTitle'>{HerbestSale.gender}</Card.Title>
                                         {/* <Card.Link href="#">Card Link</Card.Link> */}
                                     </Card.Body>
                                     </Card>
--- frontend/src/Components/NewArrivals/NewArrivals.jsx
+++ frontend/src/Components/NewArrivals/NewArrivals.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './NewArrivals.css'
+import { Link } from 'react-router-dom';
 
 const NewArrivals = () => {
     const [newArrivals , setnewArrivals] = useState([]);
@@ -19,18 +20,18 @@ const NewArrivals = () => {
       
 
         const simulatedImages = [
-            { image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'C' },
-            { image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'B' },
-            { image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A' },
-            { image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=1024x1024&w=is&k=20&c=iGtRKCTRSvPVl3eOIpzzse5SvQFfImkV0TZuFh-74ps=', gender: 'B' },
-            { image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'A' },
-            { image: 'https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg', gender: 'B' },
-            { image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'A' },
-            { image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'C' },
-            { image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A' },
-            { image: 'https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg', gender: 'B' },
-            { image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=1024x1024&w=is&k=20&c=iGtRKCTRSvPVl3eOIpzzse5SvQFfImkV0TZuFh-74ps=', gender: 'A' },
-            { image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'C' },
+            {id : '1' , image: 'https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg?auto=compress&cs=tinysrgb&w=400',gender : 'C' },
+            {id : '2' , image: 'https://pxbar.com/wp-content/uploads/2023/09/hidden-girl-pic.jpg', gender: 'B' },
+            {id : '3' , image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A' },
+            {id : '4' , image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=1024x1024&w=is&k=20&c=iGtRKCTRSvPVl3eOIpzzse5SvQFfImkV0TZuFh-74ps=', gender: 'B' },
+            {id : '5' , image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'A' },
+            {id : '6' , image: 'https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg', gender: 'B' },
+            {id : '7' , image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'A' },
+            {id : '8' , image: 'https://images.pexels.com/photos/36029/aroni-arsa-children-little.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500', gender: 'C' },
+            {id : '10' , image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'A' },
+            {id : '11' , image: 'https://e1.pxfuel.com/desktop-wallpaper/903/679/desktop-wallpaper-97-aesthetic-best-profile-pic-for-instagram-for-boy-instagram-dp-boys.jpg', gender: 'B' },
+            {id : '12' , image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=1024x1024&w=is&k=20&c=iGtRKCTRSvPVl3eOIpzzse5SvQFfImkV0TZuFh-74ps=', gender: 'A' },
+            {id : '13' , image: 'https://w0.peakpx.com/wallpaper/607/199/HD-wallpaper-evening-pic-natura-thumbnail.jpg', gender: 'C' },
           ];
       
           
@@ -57,7 +58,7 @@ const NewArrivals = () => {
                             .slice(0, 3)
                             .map((newArrival, index) => (
                             <div key={index} className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center newArrivalImg">
-                                <img src={newArrival.image} className="img-fluid m-2" alt="" />
+                                <Link to={`/product/${newArrival.id}`}><img src={newArrival.image} className="img-fluid m-2" alt="" /></Link>
                             </div>
                             ))}
                         </div>
@@ -72,7 +73,7 @@ const NewArrivals = () => {
                             .slice(0, 3)
                             .map((newArrival, index) => (
                             <div key={index} className="col-lg-4 col-md-6 col-12 d-flex justify-content-center align-items-center newArrivalImg">
-                                <img src={newArrival.image} className="img-fluid m-2" alt="" />
+                                <Link to={`/product/${newArrival.id}`}><img src={newArrival.image} className="img-fluid m-2" alt="" /></Link>
                             </div>
                             ))}
                         </div>
