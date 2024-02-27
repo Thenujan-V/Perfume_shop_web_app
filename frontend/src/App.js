0a280c7ee4a92003dd9f217011ab3d4f6e99443a
Nilushiya <nilushiyak@gmail.com>
2024-02-27 23:50:57 +0530

    product display build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,9 @@ import Cart from "./Pages/Cart";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Register/Register";
 // import Nav from '../Components/Nav/Nav'
-import ShopCategory from "./Pages/ShopCategory/ShopCategory";
-
-
 import About from "./Pages/About";
-import ShowProducts from "./Pages/ShowProducts";
-import Shop from "./Pages/shop";
+import Shop from "./Pages/FilterPage";
 
-
-// import Cart from "./Pages/Cart";
-
-// import Login from "./Pages/Login/Login";
-// import Register from "./Pages/Register/Register";
 
 
 function App() {
@@ -24,35 +15,16 @@ function App() {
     <div className="App">
       <BrowserRouter>
       {/* <Nav /> */}
-        <Routes>
-          <Route path="/shop" element={<Shop />}/>
-          {/* <Route path="/cart" element = {<Cart />}/> */}
-          {/* <Route path="/shop" element={<ShopCategory />}>
-            <Route path=":productGender" element={<ShopCategory banner={}/>}></Route>
-          </Route> */}
-          {/* <Route path="/showProducts" element={<ShowProducts />}/>
+        <Routes>   
           <Route path="/" element = {<Home />}/>
+          <Route path="/login" element = {<Login />}/>
+          <Route path="/register" element = {<Register />}/>
+          <Route path="/shop" element={<Shop />}/>  
           <Route path="/cart" element = {<Cart />}/>
-<<<<<<< HEAD
-          
-          <Route path="/login" element = {<Login />}/>
-          <Route path="/register" element = {<Register />}/>
-          <Route path="/about" element = {<About />}/> */}
-
-      {/* <Route path="/product" element={<Product />}>
-=======
-         
-          <Route path="/login" element = {<Login />}/>
-          <Route path="/register" element = {<Register />}/>
-          <Route path="/" element = {<Home />}/>
           <Route path="/about" element = {<About />}/>
-
-          {/* <Route path="/product" element={<Product />}>
->>>>>>> 1646534b8e9deca1638b91c5f332f6995405045b
->>>>>>> f4389a20d8b3ea8cd5dad6b7e247c1b719ce695c
-          <Route path=":productId" element={<Product />}/>
-
-          </Route> */}
+          <Route path="/product" element={<Product />}>
+              <Route path=":productId" element={<Product />}/>
+          </Route> 
         </Routes>
       </BrowserRouter>
     </div>
