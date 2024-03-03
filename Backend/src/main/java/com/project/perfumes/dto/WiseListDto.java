bf1f35876cf4f4d70967b474f0fba10f6fa635b9
Ragapiriya <r2001piriya@gmail.com>
2024-03-03 10:32:59 +0530

    Wishlist backend updated

---
+++ Backend/src/main/java/com/project/perfumes/dto/WiseListDto.java
@@ -0,0 +1,16 @@
+package com.project.perfumes.dto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class WiseListDto {
+    private Long wId;
+    private Long pId;
+    private Long uId;
+}
